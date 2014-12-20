745aec533d6eff066a04bd1c5f3791c29aa82677
mibaldi <mibaldi2@gmail.com>
2014-12-20 11:03:24 +0100

    Agregados agentes de otros equipos y creados bat de pruebas

--- rundeavsRocosos.bat
+++ rundeavsRocosos.bat
@@ -1,8 +1,8 @@
 @cd /D %~dp0
 @cd jgomas/bin/mas
-start  jgomas_manager.bat
+start jgomas_manager.bat
 @timeout 5
-start  jgomas_launcher-PruebaDefensa.bat > RocososvsJGOMAS.txt
+start jgomas_launcher-deaVSrocosos.bat
 @cd ../render/w32
 @timeout 5
 @run_jgomasrender
